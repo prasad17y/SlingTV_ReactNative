a56041b19d3b56224cc155e2643a328a9b619852
prasad17y <42862956+prasad17y@users.noreply.github.com>
2020-11-28 00:25:43 +0530

    Second commit

---
+++ src/api_results/data.js
@@ -0,0 +1,211 @@
+export function getMockData(){
+    return {
+        "menuItems" : [
+            "ALL",
+            "MY WATCHLIST",
+            "MOVIES",
+            "TV SHOWS",
+            "RENTALS",
+            "KIDS"
+        ],
+        "rows" : [
+            {
+                "id":"1",
+                "type":"0",
+                "title":"My Watch List"
+            },
+            {
+                "id":"2",
+                "type":"0",
+                "title":"My Recent Recordings"
+            },
+            {
+                "id":"3",
+                "type":"0",
+                "title":"Top picks for you"
+            }
+        ],
+        "shows" : [
+            {
+                "id":"1",
+                "type":"0",
+                "name":"Cuomo Prime Time",
+                "channel_name":"CNN",
+                "length":"1h",
+                "rating":"TV-PG",
+                "image_url":"https://cdn.cnn.com/cnnnext/dam/assets/180522175822-cuomo-prime-time-card-super-tease.jpg"
+            },
+            {
+                "id":"2",
+                "type":"0",
+                "name":"The Daily Show",
+                "channel_name":"Comedy Central",
+                "length":"35m",
+                "rating":"TV-PG",
+                "image_url":"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMFCPaj81_Rm6k322JZnsRK51UzUMeBtsnUA&usqp=CAU"
+            },
+            {
+                "id":"3",
+                "type":"0",
+                "name":"South Park",
+                "channel_name":"Comedy Central",
+                "length":"20m",
+                "rating":"TV-MA",
+                "image_url":"https://www.inquirer.com/resizer/I44aYtLwpUNyoKj3uE-U8bBIdXQ=/1400x932/smart/arc-anglerfish-arc2-prod-pmn.s3.amazonaws.com/public/HVWTZB4CNJBDXMWKGMXAWU2BFY.jpg"
+            },
+            {
+                "id":"4",
+                "type":"0",
+                "name":"Mr. Robot",
+                "channel_name":"USA Network",
+                "length":"35m",
+                "rating":"TV-PG",
+                "image_url":"https://images.newindianexpress.com/uploads/user/imagelibrary/2019/4/29/w900X450/Dl2avGTWwAAT-a3.jpg"
+            },
+            {
+                "id":"5",
+                "type":"0",
+                "name":"The Sinner",
+                "channel_name":"USA Network",
+                "length":"40m",
+                "rating":"Not rated",
+                "image_url":"https://upload.wikimedia.org/wikipedia/en/9/99/The_Sinner.jpg"
+            },
+            {
+                "id":"6",
+                "type":"0",
+                "name":"Suits",
+                "channel_name":"USA Network",
+                "length":"40m",
+                "rating":"TV-PG",
+                "image_url":"https://images.indianexpress.com/2017/06/suits-7591.jpg"
+            },
+            {
+                "id":"7",
+                "type":"0",
+                "name":"Modern Family",
+                "channel_name":"USA Network",
+                "length":"30m",
+                "rating":"TV-14",
+                "image_url":"https://lh3.googleusercontent.com/proxy/uTbzfIjkSYM76ph0MTSc_GCI8T6cryN_5VEyHuQwyNJUhlN4fpT11WfIr5PfLCgzQTWmxAQXCOTPhLd5svmH6e93tuQyp2NDjUiPhPnh1ljc"
+            },
+            {
+                "id":"8",
+                "type":"0",
+                "name":"Thursday Night Football",
+                "channel_name":"NFL Network",
+                "length":"1h30m",
+                "rating":"TV-PG",
+                "image_url":"https://images.daznservices.com/di/library/sporting_news/4a/88/thursday-night-football-090920-ftr_vac7jjijoaax1fo2v1x42aysl.jpg?t=1997799588&quality=100&w=1280&h=720"
+            },
+            {
+                "id":"9",
+                "type":"0",
+                "name":"Inside The NFL",
+                "channel_name":"NFL Network",
+                "length":"1h",
+                "rating":"TV-PG",
+                "image_url":"https://m.media-amazon.com/images/M/MV5BYzczOGQyZDItOTlkMC00ZTA2LTk3NTUtNDQ4NTUxZmY0ZDY5XkEyXkFqcGdeQXRyYW5zY29kZS13b3JrZmxvdw@@._V1_.jpg"
+            },
+            {
+                "id":"10",
+                "type":"0",
+                "name":"NFL Total Access",
+                "channel_name":"NFL Network",
+                "length":"1h",
+                "rating":"TV-PG",
+                "image_url":"https://cdn.tvpassport.com/image/show/960x540/25202.jpg"
+            },
+            {
+                "id":"11",
+                "type":"0",
+                "name":"Counting Cars",
+                "channel_name":"History",
+                "length":"40m",
+                "rating":"TV-PG",
+                "image_url":"https://upload.wikimedia.org/wikipedia/en/6/6d/CountingCarsLogoCast.png"
+            },
+            {
+                "id":"12",
+                "type":"0",
+                "name":"Vikings",
+                "channel_name":"History",
+                "length":"50m",
+                "rating":"TV-14",
+                "image_url":"https://gamerrewind.com/wp-content/uploads/2020/05/vikings-1200x900-1.png"
+            },
+            {
+                "id":"13",
+                "type":"0",
+                "name":"NBA on ESPN",
+                "channel_name":"ESPN",
+                "length":"2h",
+                "rating":"TV-PG",
+                "image_url":"https://images2.minutemediacdn.com/image/upload/c_fill,w_720,ar_16:9,f_auto,q_auto,g_auto/shape/cover/sport/espn-nba-46afe06d6f1060ec7ee1615e57ddb058.jpg"
+            },
+            {
+                "id":"14",
+                "type":"0",
+                "name":"SportsCenter",
+                "channel_name":"ESPN",
+                "length":"40m",
+                "rating":"TV-PG",
+                "image_url":"https://www.rapidtvnews.com/images/2019/May_2019_/sportscenter-3-may-2019.jpg"
+            },
+            {
+                "id":"15",
+                "type":"0",
+                "name":"Monday Night Football",
+                "channel_name":"ESPN",
+                "length":"2h",
+                "rating":"TV-PG",
+                "image_url":"https://images.daznservices.com/di/library/sporting_news/34/8c/monday-night-football-090920-getty_fd2ersjgsu8v1kaggs19z9s17.jpg?t=-1969667132&quality=100"
+            },
+            {
+                "id":"16",
+                "type":"0",
+                "name":"ESPN College Football",
+                "channel_name":"ESPN",
+                "length":"1h30m",
+                "rating":"TV-PG",
+                "image_url":"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/01730a45857929.583f4722a164c.jpg"
+            },
+            {
+                "id":"17",
+                "type":"0",
+                "name":"First Take",
+                "channel_name":"ESPN",
+                "length":"1h",
+                "rating":"TV-PG",
+                "image_url":"https://espnpressroom.com/us/files/2017/01/ft-780x400.png"
+            },
+            {
+                "id":"18",
+                "type":"0",
+                "name":"ESPN Major League Baseball",
+                "channel_name":"ESPN",
+                "length":"2h",
+                "rating":"TV-PG",
+                "image_url":"https://www.sportsmediawatch.com/wp-content/uploads/2013/04/mlbespn.png"
+            },
+            {
+                "id":"19",
+                "type":"0",
+                "name":"CNN Newsroom Live",
+                "channel_name":"CNN",
+                "length":"1h",
+                "rating":"TV-PG",
+                "image_url":"https://cdn.cnn.com/cnnnext/dam/assets/150803125451-newsroom-card-august-2015-update-super-tease.jpg"
+            },
+            {
+                "id":"20",
+                "type":"0",
+                "name":"Erin Burnet Outfront",
+                "channel_name":"CNN",
+                "length":"1h",
+                "rating":"TV-PG",
+                "image_url":"https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/60b0a529856393.560771b50862a.jpg"
+            }      
+        ]
+    };
+}
